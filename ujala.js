c60e93b1f353a6515f145bec6848716dbb0eb96b
Shaunak Bhanarkar <shaunak.bhanarkar@gmail.com>
2021-08-09 22:19:29 +0530

    ujala quick links

---
+++ ujala.js
@@ -0,0 +1,33 @@
+
+
+$(document).ready(function () {
+
+    $('#nav-background').on('click' , function() {
+        navigate('#background');
+    });
+
+
+    $('#nav-challenge').on('click' , function() {
+        navigate('#challenge');
+    });
+
+    $('#nav-solution').on('click' , function() {
+        navigate('#solution');
+    });
+
+    $('#nav-phase-1').on('click' , function() {
+        navigate('#phase-1');
+    });
+
+    $('#nav-phase-2').on('click' , function() {
+        navigate('#phase-2');
+    });
+
+    $('#nav-reflection').on('click' , function() {
+        navigate('#reflection');
+    });
+
+
+
+});
+
