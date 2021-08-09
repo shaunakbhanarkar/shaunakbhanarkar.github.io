018bb22edc87a0454b225f0834aad214e7fe4c93
Shaunak Bhanarkar <shaunak.bhanarkar@gmail.com>
2021-08-09 20:48:02 +0530

    ios quick links

---
+++ iosnotifications.js
@@ -0,0 +1,37 @@
+
+
+$(document).ready(function () {
+
+    $('#nav-background').on('click' , function() {
+        navigate('#background');
+    });
+
+
+    $('#nav-challenge').on('click' , function() {
+        navigate('#challenge');
+    });
+
+    $('#nav-user-research').on('click' , function() {
+        navigate('#user-research');
+    });
+
+    $('#nav-competitive-research').on('click' , function() {
+        navigate('#competitive-research');
+    });
+
+    $('#nav-issues').on('click' , function() {
+        navigate('#issues');
+    });
+
+    $('#nav-wireframes').on('click' , function() {
+        navigate('#wireframes');
+    });
+
+
+    $('#nav-mockups').on('click' , function() {
+        navigate('#mockups');
+    });
+
+
+});
+
--- project.js
+++ project.js
@@ -1,3 +1,6 @@
+
+
+
 var navigate = function(target){
 
     $('html, body').animate({
@@ -8,41 +11,11 @@ var navigate = function(target){
 
 $(document).ready(function () {
 
-    $('#nav-background').on('click' , function() {
-        navigate('#background');
-    });
-
-    $('#nav-problem').on('click' , function() {
-        navigate('#problem');
-    });
-
-    $('#nav-solution').on('click' , function() {
-        navigate('#solution');
-    });
-
-    $('#nav-design-process').on('click' , function() {
-        navigate('#design-process');
-    });
-
-    $('#nav-research').on('click' , function() {
-        navigate('#research');
-    });
-
-    $('#nav-conception-and-design').on('click' , function() {
-        navigate('#conception-and-design');
-    });
-
-    $('#nav-testing').on('click' , function() {
-        navigate('#testing');
-    });
-
-    $('#nav-final-prototype').on('click' , function() {
-        navigate('#final-prototype');
-    });
-
     $('#nav-top').on('click' , function() {
         navigate('#header');
     });
 
 
 });
+
+
