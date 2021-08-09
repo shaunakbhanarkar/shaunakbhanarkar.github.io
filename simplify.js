85acc9d7fba1063f9a66997d486a7248127a9077
Shaunak Bhanarkar <shaunak.bhanarkar@gmail.com>
2021-08-09 21:01:26 +0530

    simplify quick links

---
+++ simplify.js
@@ -0,0 +1,33 @@
+
+
+$(document).ready(function () {
+
+    $('#nav-background').on('click' , function() {
+        navigate('#background');
+    });
+
+
+    $('#nav-problem-statement').on('click' , function() {
+        navigate('#problem-statement');
+    });
+
+    $('#nav-leap-motion-controller').on('click' , function() {
+        navigate('#leap-motion-controller');
+    });
+
+    $('#nav-setup').on('click' , function() {
+        navigate('#setup');
+    });
+
+    $('#nav-experiment').on('click' , function() {
+        navigate('#experiment');
+    });
+
+    $('#nav-further-scope').on('click' , function() {
+        navigate('#further-scope');
+    });
+
+
+
+});
+
