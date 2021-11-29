9547636decf54b4385cfe50d686b8a641e2cef88
Shaunak Bhanarkar <shaunak.bhanarkar@gmail.com>
2021-11-29 12:16:57 +0530

    reduce number of projects

--- home.js
+++ home.js
@@ -6,29 +6,29 @@ $(document).ready(function () {
         navigate('#work');
     });
 
-    $('#ux-tab').on('click' , function() {
-        $('.tab-active').removeClass('tab-active');
-        $('#ux-tab').addClass('tab-active');
+    // $('#ux-tab').on('click' , function() {
+    //     $('.tab-active').removeClass('tab-active');
+    //     $('#ux-tab').addClass('tab-active');
 
-        $('.project-container-js').addClass('hidden');
-        $('#ux-projects').removeClass('hidden');
-    });
+    //     $('.project-container-js').addClass('hidden');
+    //     $('#ux-projects').removeClass('hidden');
+    // });
 
-    $('#branding-tab').on('click' , function() {
-        $('.tab-active').removeClass('tab-active');
-        $('#branding-tab').addClass('tab-active');
+    // $('#branding-tab').on('click' , function() {
+    //     $('.tab-active').removeClass('tab-active');
+    //     $('#branding-tab').addClass('tab-active');
 
-        $('.project-container-js').addClass('hidden');
-        $('#branding-projects').removeClass('hidden');
-    });
+    //     $('.project-container-js').addClass('hidden');
+    //     $('#branding-projects').removeClass('hidden');
+    // });
 
-    $('#tech-tab').on('click' , function() {
-        $('.tab-active').removeClass('tab-active');
-        $('#tech-tab').addClass('tab-active');
+    // $('#tech-tab').on('click' , function() {
+    //     $('.tab-active').removeClass('tab-active');
+    //     $('#tech-tab').addClass('tab-active');
 
-        $('.project-container-js').addClass('hidden');
-        $('#tech-projects').removeClass('hidden');
-    });
+    //     $('.project-container-js').addClass('hidden');
+    //     $('#tech-projects').removeClass('hidden');
+    // });
 
 
 });
