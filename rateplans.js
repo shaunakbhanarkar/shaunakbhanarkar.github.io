816aadbf35da71917e16fb2d9d662da7ae45d310
Shaunak Bhanarkar <shaunak.bhanarkar@gmail.com>
2022-10-03 20:24:49 -0400

    rate plans side navigation

---
+++ rateplans.js
@@ -0,0 +1,41 @@
+
+
+$(document).ready(function () {
+
+    $('#nav-background').on('click' , function() {
+        navigate('#background');
+    });
+
+
+    $('#nav-challenge').on('click' , function() {
+        navigate('#challenge');
+    });
+
+    $('#nav-competitive-research').on('click' , function() {
+        navigate('#competitive-research');
+    });
+
+    $('#nav-constraints').on('click' , function() {
+        navigate('#constraints');
+    });
+
+    $('#nav-iterations').on('click' , function() {
+        navigate('#iterations');
+    });
+
+    $('#nav-user-testing').on('click' , function() {
+        navigate('#user-testing');
+    });
+
+    $('#nav-final-designs').on('click' , function() {
+        navigate('#final-designs');
+    });
+
+    $('#nav-reflection').on('click' , function() {
+        navigate('#reflection');
+    });
+
+
+
+});
+
