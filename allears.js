248bbb1e6d8624fc1972b20256d3f3b33fdef92c
Shaunak Bhanarkar <shaunak.bhanarkar@gmail.com>
2022-10-04 23:22:59 -0400

    all ears quick links

---
+++ allears.js
@@ -0,0 +1,41 @@
+
+
+$(document).ready(function () {
+
+    $('#nav-background').on('click' , function() {
+        navigate('#background');
+    });
+
+
+    $('#nav-wireframes').on('click' , function() {
+        navigate('#wireframes');
+    });
+
+    $('#nav-color-palette').on('click' , function() {
+        navigate('#color-palette');
+    });
+
+    $('#nav-typography').on('click' , function() {
+        navigate('#typography');
+    });
+
+    $('#nav-design-system').on('click' , function() {
+        navigate('#design-system');
+    });
+
+    $('#nav-layout').on('click' , function() {
+        navigate('#layout');
+    });
+
+    $('#nav-final-design').on('click' , function() {
+        navigate('#final-design');
+    });
+
+    $('#nav-reflection').on('click' , function() {
+        navigate('#reflection');
+    });
+
+
+
+});
+
