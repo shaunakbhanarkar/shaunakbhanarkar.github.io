9490c44491af6fda09ae2e07c504be94a5a7bfb8
Shaunak Bhanarkar <shaunak.bhanarkar@gmail.com>
2023-01-14 16:41:23 -0500

    oyo link sidebar nav

---
+++ oyolink.js
@@ -0,0 +1,41 @@
+
+
+$(document).ready(function () {
+
+    $('#nav-background').on('click' , function() {
+        navigate('#background');
+    });
+
+
+    $('#nav-challenge').on('click' , function() {
+        navigate('#challenge');
+    });
+
+    $('#nav-design-opportunities').on('click' , function() {
+        navigate('#design-opportunities');
+    });
+
+    $('#nav-wireframes').on('click' , function() {
+        navigate('#wireframes');
+    });
+
+    $('#nav-components').on('click' , function() {
+        navigate('#components');
+    });
+
+    $('#nav-layout').on('click' , function() {
+        navigate('#layout');
+    });
+
+    $('#nav-final-designs').on('click' , function() {
+        navigate('#final-designs');
+    });
+
+    $('#nav-reflection').on('click' , function() {
+        navigate('#reflection');
+    });
+
+
+
+});
+
